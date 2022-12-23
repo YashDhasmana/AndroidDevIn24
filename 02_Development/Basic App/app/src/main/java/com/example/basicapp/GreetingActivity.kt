ff987b86f0be9e82c820dd9d76b8b9dc5d72f9ff
YashDhasmana <yashdhasmana03@gmail.com>
2022-12-23 17:12:01 +0530

    Code Files

---
+++ 02_Development/Basic App/app/src/main/java/com/example/basicapp/GreetingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.basicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GreetingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_greeting)
+    }
+}
---
+++ 02_Development/Basic App/app/src/main/java/com/example/basicapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.basicapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun createCard(view: View) {
+
+        val intent = Intent(this, GreetingActivity::class.java)
+        startActivity(intent)
+
+    }
+
+}
+
